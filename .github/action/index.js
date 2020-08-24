4cf97de8bd8b343238a584a3f8cdeb5116bb4f31
Sam Donald <sam@mudlabs.com.au>
2020-08-24 14:24:26 +1000

    Try again

--- .github/action/index.js
+++ .github/action/index.js
@@ -5,6 +5,9 @@ try {
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`Hi ${github.context.actor}`);
   console.log(payload)
+  const octokit = github.getOctokit(env.GITHUB_TOKEN)
+  octokit.issues.getComment(github.context.payload.comment.id).then(val => console.log(`comment value`, val)).catch(err => log(err))
+
 } catch (error) {
   core.setFailed(error.message);
 }

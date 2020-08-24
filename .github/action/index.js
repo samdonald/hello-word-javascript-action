5bef310eba3b76203bcee63ea34509ebebe9b7e8
Sam Donald <sam@mudlabs.com.au>
2020-08-24 13:59:14 +1000

    Try adding if state to workflow job

--- .github/action/index.js
+++ .github/action/index.js
@@ -2,12 +2,9 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 try {
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${github.context.actor}`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
+  console.log(`Hi ${github.context.actor}`);
+  console.log(payload)
 } catch (error) {
   core.setFailed(error.message);
 }

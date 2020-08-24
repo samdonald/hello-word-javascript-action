b8787bb06ae115ac794bd4cdefa24f7b4f3bdb9b
Sam Donald <sam@mudlabs.com.au>
2020-08-24 18:14:59 +1000

    Trying again

--- .github/action/index.js
+++ .github/action/index.js
@@ -1,6 +1,11 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
 
+function getProjectTitle(body) {
+
+  return title;
+}
+
 try {
   const body = github.context.payload.issue.body
   const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
@@ -9,20 +14,24 @@ try {
   const repo = github.context.payload.repository.name;
   const issue_number = github.context.payload.issue.number;
   const contributor = github.context.payload.sender.login;
-
-  octokit.issues.update({ 
-    owner,
-    repo,
-    issue_number,
-    title: `[project] Arrow Bone by @${contributor}`
-  })
   
+  const project_title = getProjectTitle(body);
+
+
   octokit.issues.createComment({
     owner,
     repo,
     issue_number,
     body: `@${contributor} Thanks for contributing!` 
   }).then(({data, headers, status}) => console.log(status, data))
+    .then(() => octokit.issues.update({
+      owner,
+      repo,
+      issue_number,
+      assignees: [owner],
+      labels: ["new project"],
+      title: `[project] ${project_title} by @${contributor}`
+    }))
   
 } catch (error) {
   core.setFailed(error.message);

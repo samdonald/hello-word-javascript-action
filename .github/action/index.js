aae4b7aa2ab2364740583ca6753f0891cf40ebf5
Sam Donald <sam@mudlabs.com.au>
2020-08-24 14:40:47 +1000

    Trying again

--- .github/action/index.js
+++ .github/action/index.js
@@ -1,9 +1,10 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const { GitHub } = require("@actions/github/lib/utils");
 
 try {
   const payload = JSON.stringify(github.context.payload, undefined, 2);
-  const octokit = github.getOctokit(github.token);
+  const octokit = new GitHub(github.token);
   console.log(`Hi ${github.context.actor}`);
   console.log(payload)
   octokit.issues.getComment(github.context.payload.comment.id).then(val => console.log(`comment value`, val)).catch(err => log(err))

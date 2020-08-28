89ba35ad7576b758aa36b0156c7b94e4a4f3e81e
Sam Donald <sam@mudlabs.com.au>
2020-08-28 12:38:57 +1000

    try update after lock

--- .github/action/index.js
+++ .github/action/index.js
@@ -26,14 +26,25 @@ try {
       return title;
     }
 
-    // throw new Error("Invalid Title");
+    throw new Error("Invalid Title");
   }
 
   (async function buildProject(body) {
     try {
+      // determin what event triggered the action
+      console.log(github.context)
       const title = parseTitle(body);
-      const locked = await octokit.issues.lock({owner,repo,issue_number});
-      await octokit.issues.update({owner,repo,issue_number,title:"Updated after locked"})
+      const ios = parseIos(body);
+      const android = parseAndroid(body);
+      const description = parseDescription(body);
+      const resources = parseResources(body);
+      const playgrounds = parsePlaygrounds(body);
+
+      if (ios == false && android == false) {
+        // request platform support status
+      }
+      
+      await octokit.issues.lock({owner,repo,issue_number});
     } catch (error) {
       switch (error.message) {
         case "Invalid Title":
@@ -54,67 +65,3 @@ try {
 } catch (error) {
   console.log(error.message);
 }
-
-// const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
-
-// function parseTitle(body) {
-//   const projectTitle = "## Project Title";
-//   const titleIndex = body.indexOf(projectTitle) + projectTitle.length;
-//   const platformsIndex = body.indexOf("## Platform Support");
-//   let title = body.substring(titleIndex, platformsIndex).trim();
-
-//   if (title.match(/^[a-z0-9 ]+$/i)) {
-//     return title;
-//   }
-
-//   throw new Error("Invalid Title");
-// }
-
-// async function buildProject() {
-//   try {
-//     console.log(github.context.payload);
-//     const contributor = github.context.payload.sender.login
-//     const body = github.context.payload.issue.body;
-//     const owner = github.context.payload.repository.owner.login;
-//     const repo = github.context.payload.repository.name;
-//     const issue_number = github.context.payload.issue.number;
-    
-//     // 1) parse the body into sections
-//     const title = parseTitle(body);
-    
-//     // 2) test each section against criteria
-
-//     // If everything in the issue template is correct let the user know the project has been accepted and is awaiting aproval
-//     const comment = await octokit.issues.createComment({
-//       owner,
-//       repo,
-//       issue_number,
-//       body: `@${contributor} thanks for the contribution. Once your playground(s) have been checked the project will be accepted.`
-//     });
-
-//     const rename = await octokit.issues.update({ 
-//       owner,
-//       repo,
-//       issue_number,
-//       title: `[project] ${title} by @${contributor}` 
-//     });
-    
-//   } catch (error) {
-//     console.log(error.message)
-//     switch (error.message) {
-//       case "Invalid Title":
-//         octokit.issues.createComment({
-//           owner,
-//           repo,
-//           issue_number,
-//           body: `@${contributor} I could not parse your title. Please reply with a short descriptive title. It should be a one liner that matches the RegExp **/^[a-z0-9 ]+$/i**\r\rThanks`
-//         })
-//         break;
-    
-//       default:
-//         break;
-//     }
-//   }
-// }
-    
-// buildProject();
